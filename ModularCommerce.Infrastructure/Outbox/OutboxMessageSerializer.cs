273229cc5af74f05e4558d58a941559938073013
marcm <marcm@DESKTOP-L1C6S0S>
2026-03-06 10:57:59 +0100

    Paso 6 — Unit of Work + Guardado automático de Domain Events + Outbox

---
+++ ModularCommerce.Infrastructure/Outbox/OutboxMessageSerializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace ModularCommerce.Infrastructure.Outbox;
+
+public static class OutboxMessageSerializer
+{
+    public static OutboxMessage Serialize(object domainEvent)
+    {
+        return new OutboxMessage
+        {
+            Id = Guid.NewGuid(),
+            Type = domainEvent.GetType().Name,
+            Content = JsonSerializer.Serialize(domainEvent),
+            OccurredOn = DateTime.UtcNow
+        };
+    }
+}
--- ModularCommerce.Infrastructure/Persistence/AppDbContext.cs
+++ ModularCommerce.Infrastructure/Persistence/AppDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ModularCommerce.Domain.Entities;
 using ModularCommerce.Infrastructure.Outbox;
+using ModularCommerce.Application.Interfaces;
+using ModularCommerce.Domain.Common;
 
 namespace ModularCommerce.Infrastructure.Persistence;
 
-public class AppDbContext : DbContext
+public class AppDbContext : DbContext, IUnitOfWork
 {
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Order> Orders => Set<Order>();
@@ -19,5 +21,28 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEntities = ChangeTracker
+            .Entries<Entity>()
+            .Where(x => x.Entity.DomainEvents.Any())
+            .ToList();
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToList();
+
+        foreach (var entity in domainEntities)
+            entity.Entity.ClearDomainEvents();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            var outboxMessage = OutboxMessageSerializer.Serialize(domainEvent);
+            OutboxMessages.Add(outboxMessage);
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
